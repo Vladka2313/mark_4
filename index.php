5175a504e5cdfcf660eb61453a1e8286bdef870e
Vladka <kot2305@ukr.net>
2016-04-26 09:06:47 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>NELSON</title>
+        <link rel="stylesheet" type="text/css" href="css/Style.css">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    </head>
+    <body>
+        <div class="content">
+        <div class="column left">
+        <IMG class="layer13" src="image/Layer 13.png"/>
+        <img class="barcopy" src="image/Bar copy.png" />
+        <h1> YOUR RESOURCE FOR GENUINE, <br> EVERYDAY STYLE.</h1>
+        <h2>INFO@AMYBNELSON.COM <br> 23	</h2>
+        <a href="#"><img class="obj objfirst" src="image/obj1.png" /></a>
+        <a href="#"><img class="obj" src="image/obj2.png" /></a>
+        <a href="#"><img class="obj" src="image/obj3.png" /></a>       
+        </div>
+        <div class="column">
+        <a href="homepages.html"><IMG class="an" src="image/AN.png"/></a>
+        <h4 class="class1">AMY B. NELSON</h4>
+        <h4 class="class2">STYLE DESIGN</h4>
+        <img class="barcopy2" src="image/Bar copy.png" />
+        <ul>
+        <li><a href="services.html">SERVICES</a></li>
+        <li><a href="team.html">STYLE TEAM</a></li>
+        <li><a href="contact.html">CONTACT</a></li>
+        <li><a href="test.php">BLOG</a></li>
+        </ul>
+        <IMG class="layer6" src="image/Layer 6.png"/>
+        </div>
+        <div class="column">
+        <IMG class="layer7" src="image/Layer 7.png"/>
+        <IMG class="layer12" src="image/Layer 12.png"/>
+        </div>
+        </div>
+    </body>
+</html>
---
+++ test.php
@@ -0,0 +1,21 @@
+<?php
+/*for($i=0;$i<10;$i++)
+echo $i ."<br>";
+$a=array (1,2,"ololo");
+for($i=0;$i<3;$i++)
+echo $a[$i]  ."<br>";
+$a=array (1,array(23,24),"ololo");
+for($i=0;$i<3;$i++)
+{
+print_r ($a[$i]) ;
+echo "<br>";
+}
+$a=array ("text0"=>1,"text1"=>2,"text2"=>"ololo");
+/*for($i=0;$i<3;$i++)
+echo $a["text".$i]  ."<br>";
+foreach ($a as $r=>$v)
+{
+	echo $r."-".$v."<br>";
+}*/
+
+?>
